d434a62530ef4d2e6cb50753404f588a0cf1da59
fazliddin <fazliddinxamdamov102@gmail.com>
2022-04-07 16:26:20 +0500

    branch

--- app-branch-service/src/main/java/com/fazliddin/appbranchservice/AppBranchServiceApplication.java
+++ app-branch-service/src/main/java/com/fazliddin/appbranchservice/AppBranchServiceApplication.java
@@ -2,12 +2,8 @@ package com.fazliddin.appbranchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
 public class AppBranchServiceApplication {
 
     public static void main(String[] args) {
